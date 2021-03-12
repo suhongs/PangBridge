56bd73418ef59e17c782c37125fc1ae65c0c37d2
윤석 한 <hys6078@naver.com>
2021-03-12 17:02:19 +0900

    test magnet, bounce, icy floor

---
+++ Assets/Scripts/Magnet.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Magnet : MonoBehaviour
+{
+    public float moveSpeed = 5f;
+
+    private void OnTriggerStay(Collider other)
+    {
+        if(other.tag=="Player")
+        {
+            other.transform.position += (transform.position - other.transform.position).normalized * Time.deltaTime * moveSpeed;
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerMove.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+    Rigidbody rigid;
+    public float moveSpeed =5f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigid = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+        rigid.velocity = new Vector3(moveHorizontal * moveSpeed, rigid.velocity.y, moveVertical * moveSpeed);
+    }
+}

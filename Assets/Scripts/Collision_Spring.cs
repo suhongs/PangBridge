82f6e4f60a58b2175be0a94cc90529b5be3ecee6
ByLeeKR <nutbooyoung@naver.com>
2021-03-13 21:15:56 +0900

    raycast_example_

---
+++ Assets/Scripts/Collision_Spring.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Collision_Spring : MonoBehaviour
+{
+    void OnCollisionEnter(Collision other)
+    { 
+	if(other.gameObject.tag == "Player")
+	{
+		Debug.Log("스프링 작동");
+		Rigidbody rigid = other.gameObject.GetComponent<Rigidbody>();
+		rigid.AddForce(new Vector3(0,1,0) * 1000f);
+	}
+    }
+}
---
+++ Assets/Scripts/Raycast_Itemcreate.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Raycast_Itemcreate : MonoBehaviour
+{
+    public GameObject SpringCylinder;
+    void Update()
+    {
+	if(Input.GetMouseButtonDown(0))
+	{
+		Debug.Log("Raycast Creation");
+		Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+		RaycastHit hit;
+		if(Physics.Raycast(ray, out hit, 100f))
+		{
+			Vector3 hitPosition = hit.point;
+			hitPosition.y = 0.5f;
+
+			GameObject item = Instantiate(SpringCylinder);
+			item.transform.position = hitPosition;	
+		}
+	}
+    }
+}

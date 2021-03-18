aada78d860875fba9e64454fa8de1c998bcb6492
qkrwlgh0369 <37234374+qkrwlgh0369@users.noreply.github.com>
2021-03-18 23:16:10 +0900

    Add demo
    데모로 만들어 놓은 Game 씬 추가

---
+++ Assets/Scripts/GoalBox.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GoalBox : MonoBehaviour
+{
+    MeshRenderer mesh;
+    Material mat;
+
+    void Start()
+    {
+        mesh = GetComponent<MeshRenderer>();
+        mat = mesh.material;
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.name == "Ball")
+        {
+            mat.color = new Color32(20, 90, 255, 181);
+        }
+    }
+}

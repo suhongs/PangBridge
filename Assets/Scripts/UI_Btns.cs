3b6afd1b0197b90db4d4b120e3309c90a8093227
윤석 한 <hys6078@naver.com>
2021-03-18 20:21:17 +0900

    add ToolBtn & ResetBtn

---
+++ Assets/Scripts/UI_Btns.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UI_Btns : MonoBehaviour
+{
+    public GameObject ToolContainer=null;
+    public GameObject Player;
+    public Transform ResetPoint;
+
+    private void Start()
+    {
+        Player = GameObject.Find("Player");
+        ResetPoint = GameObject.Find("ResetPoint").transform;
+    }
+
+    public void ToggleToolBtn()
+    {
+        ToolContainer.SetActive(!ToolContainer.activeSelf);
+    }
+    public void ResetBtn()
+    {
+        Player.transform.position = ResetPoint.position;
+    }
+}

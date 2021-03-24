25b04d1ee9d27d91d8f32d6eefaced7dc780d40b
한윤석 <hys6078@naver.com>
2021-03-24 23:39:55 +0900

    add ReverseGravity space and script

--- Assets/Scripts/Tools.cs
+++ Assets/Scripts/Tools.cs
@@ -3,29 +3,12 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
-public class Tools : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler
+public class Tools : MonoBehaviour
 {
-    public Canvas canvas = null;
-
+    Vector3 mousePosition;
+	public GameObject Tool = null;
     private void Update()
     {
-        if (Input.GetMouseButtonDown(0))
-        {
-            //canvas.gameObject.SetActive(!canvas.gameObject.activeSelf);
-        }
-    }
-    Vector3 screenToWorldPosition;
-    public void OnBeginDrag(PointerEventData eventData)
-    {
-        Debug.Log("begin drag");
-    }
-
-    public void OnDrag(PointerEventData eventData)
-    {
-        Debug.Log("dragging");
-    }
-    public void OnEndDrag(PointerEventData eventData)
-    {
-        Debug.Log("end drag");
-    }
+		
+	}
 }

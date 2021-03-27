827936cca5ca7f3055291889b3f427d0381f45fe
qkrwlgh0369 <37234374+qkrwlgh0369@users.noreply.github.com>
2021-03-27 19:04:17 +0900

    Add set tool
    도구 모음에서 원하는 도구 버튼을 누르면 오브젝트가 마우스를 따라다니고 도구가 배치되길 원하는 위치를 클릭을 하면 오브젝트가 그 위치에 배치됨.
    
    도구 버튼을 눌렀으나 배치하고 싶지 않을 때는 esc 키를 누르면 됨.
    
    + Tool, Obstacle 오브젝트 태그 추가

---
+++ Assets/Scripts/ToolsHandler.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Windows.Input;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class ToolsHandler : MonoBehaviour
+{
+    string curTool = null;
+    GameObject tool = null;
+    GameObject prefab = null;
+    bool btnClicked = false;
+    private Vector3 mousePos;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        mousePos = Camera.main.ScreenToWorldPoint(
+        new Vector3(
+        Input.mousePosition.x,
+        Input.mousePosition.y,
+        -Camera.main.transform.position.z));
+
+        // 도구 버튼 클릭
+        if (btnClicked)
+        {
+            if (tool != null)
+            {
+                // 버튼 클릭하면 해당 오브젝트가 마우스 따라 다님
+                tool.transform.position = mousePos;
+            }
+
+
+            if (Input.GetMouseButtonDown(0))
+            { 
+                if (tool != null)
+                {
+                    // 클릭하면 따라다니던 오브젝트 그 위치에 고정
+
+                    // !! 보드 위가 아니거나 다른 오브젝트와 겹치는 경우 놓이지 않게 하는 기능 추가해야 함 !!
+                    tool = null;
+                    btnClicked = false;
+                }
+            }
+
+            if (Input.GetKeyDown(KeyCode.Escape))
+            {
+                // 배치하기 전에 esc 키를 누르면 오브젝트 취소
+                btnClicked = false;
+                if(tool != null)
+                    Destroy(tool);
+            }
+        }
+    }
+
+    public void OnClick()
+    {
+        GameObject tempBtn = EventSystem.current.currentSelectedGameObject;
+
+        if (!btnClicked)
+        {
+            btnClicked = true;
+            curTool = tempBtn.name;
+            Debug.Log(curTool);
+            // 버튼 이름으로 프리팹 불러옴
+            prefab = Resources.Load("Prefab/" + curTool) as GameObject;
+            tool = MonoBehaviour.Instantiate(prefab, new Vector3(0, 0, 0), Quaternion.identity) as GameObject;
+            tool.tag = "Tool";
+            // 리셋 버튼 누르면 태그가 Tool인 오브젝트 없애고 돈 환불처리 시키기
+        }
+    }
+}

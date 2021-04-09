f3a8b5aeedf988dc45670d287586e53434c13af4
qkrwlgh0369 <37234374+qkrwlgh0369@users.noreply.github.com>
2021-04-10 02:34:32 +0900

    add stop button

--- Assets/Scripts/UI_Btns.cs
+++ Assets/Scripts/UI_Btns.cs
@@ -9,12 +9,15 @@ public class UI_Btns : MonoBehaviour
     public GameManager gm;
     public GameObject SettingContainer;
     public GameObject StartButton = null;
+    public GameObject StopButton = null;
+
     private Rigidbody rb;
     private void Start()
     {
         Player = GameObject.Find("Player");
         gm = GameObject.Find("GameManager").GetComponent<GameManager>();
         rb = Player.GetComponent<Rigidbody>();
+        StopButton.SetActive(false);
     }
 
     public void StartBtn()
@@ -22,7 +25,23 @@ public class UI_Btns : MonoBehaviour
         rb.useGravity = true;
         Player.GetComponent<SphereCollider>().enabled = true;
         StartButton.SetActive(false);
+        StopButton.SetActive(true);
     }
+
+    public void StopBtn()
+    {
+        Player.transform.position = gm.ResetPoint.position; //플레이어를 reset point로 이동
+        rb.useGravity = false; //플레이어 중력작용x
+        rb.velocity = Vector3.zero; //플레이어에게 받던 힘x
+        Player.GetComponent<SphereCollider>().enabled = false; //플레이어 collider 끔
+
+        gm.InitializeStar();
+        gm.CurrentCoin.text = gm.StartCoin.ToString(); //코인 초기화
+        StartButton.SetActive(true);
+        StopButton.SetActive(false);
+        Destroy(Player.gameObject.GetComponent<ConstantForce>());
+    }
+
     public void ResetBtn()
     {
         GameObject [] tools = GameObject.FindGameObjectsWithTag("Tool"); //Tool태그의 오브젝트 검색 후 배열에 담음
@@ -39,8 +58,10 @@ public class UI_Btns : MonoBehaviour
 
         gm.InitializeStar();
         gm.CurrentCoin.text = gm.StartCoin.ToString(); //코인 초기화
-        StartButton.SetActive(true); //start button 제거
+        StartButton.SetActive(true);
+        StopButton.SetActive(false);
         Destroy(Player.gameObject.GetComponent<ConstantForce>());
+
     }
 
     public void SellBtn()

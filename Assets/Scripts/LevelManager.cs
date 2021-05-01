2b50615766ac54f396dc08382c304551ec876f59
qkrwlgh0369 <37234374+qkrwlgh0369@users.noreply.github.com>
2021-05-01 22:49:03 +0900

    add scene, animation
    메인화면, 레벨 선택, ui버튼 애니메이션 추가

---
+++ Assets/Scripts/LevelManager.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class LevelManager : MonoBehaviour
+{
+    public int[] LevelScore;
+    public int level;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // 1,2,3 => 스코어, 0 => 현재 스테이지, -1 => 잠김
+
+        // 초기 설정
+        LevelScore = new int[] { 0, -1, -1, -1, -1 };
+
+        // 스테이지 스코어 불러옴
+         LevelScore = new int[] { 3, 2, 0, -1, -1 };
+
+
+        for (int i=0; i < LevelScore.Length; i++)
+        {
+            int curLevel = i + 1;
+            GameObject levelObject = GameObject.Find(curLevel.ToString());
+
+            if(LevelScore[i] > 0)
+            {
+                Image scoreImage = levelObject.transform.GetChild(1).GetComponent<Image>();
+                scoreImage.sprite = Resources.Load<Sprite>("star"+LevelScore[i]);
+                scoreImage.color = new Color(1, 1, 1, 1);
+            }
+
+            if (LevelScore[i] < 0)
+            {
+                Button levelBtn = levelObject.transform.GetChild(0).GetComponent<Button>();
+                levelBtn.interactable = false;
+            }
+        }
+    }
+
+    public void BtnClicked()
+    {
+        string level_s = EventSystem.current.currentSelectedGameObject.name;
+        if (level_s == "1") level = 1;
+        if (level_s == "2") level = 2;
+        if (level_s == "3") level = 3;
+        if (level_s == "4") level = 4;
+        if (level_s == "5") level = 5;
+    }
+}
---
+++ Assets/Scripts/SceneChange.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneChange : MonoBehaviour
+{
+    public Animator animator;
+    private int levelToLoad;
+
+    public void StartBtnClicked()
+    {
+        // 메인씬 -> 레벨 선택씬
+        FadeToLevel(1);
+    }
+
+    public void LevelBtnClicked()
+    {
+        // 레벨 선택씬 -> 게임씬
+        GameObject.Find("Canvas").GetComponent<LevelManager>().BtnClicked();
+        FadeToLevel(2);
+    }
+
+    public void LevelFinished()
+    {
+        FadeToLevel(2);
+    }
+
+    public void FadeToLevel (int levelIndex)
+    {
+        levelToLoad = levelIndex;
+        animator.SetTrigger("FadeOut");
+    }
+
+    public void OnFadeComplete()
+    {
+        SceneManager.LoadScene(levelToLoad);
+    }
+}

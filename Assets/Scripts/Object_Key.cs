bc5a7890be3e96c04a18c5b70ae9816e0201a3d0
한윤석 <hys6078@naver.com>
2021-05-12 18:25:03 +0900

    modify han_ver2

---
+++ Assets/Scripts/Object_Key.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Object_Key : MonoBehaviour
+{
+
+    private Vector3 defaultPosition;
+    private Vector3 defaultScale;
+    private bool isGameStarted;
+
+    void Start()
+    {
+        isGameStarted = true;
+        defaultScale = transform.localScale;
+        defaultPosition = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (GameManager.isGaming)
+        {
+            if (isGameStarted)
+            {
+                gameObject.GetComponent<Rigidbody>().useGravity = true;
+                isGameStarted = false;
+            }
+        }
+        else if (GameManager.isGaming == false)
+        {
+            if (!isGameStarted) //게임이 시작된 적이 있는데 정지됬다면
+            {
+                transform.position = defaultPosition;
+                transform.localScale = defaultScale;
+                gameObject.GetComponent<Rigidbody>().useGravity = false;
+                isGameStarted = true;
+            }
+            defaultPosition = transform.position; //게임 중이 아닐 땐 지속적으로 위치 갱신
+        }
+    }
+
+}

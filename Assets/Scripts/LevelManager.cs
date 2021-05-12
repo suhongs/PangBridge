f58ab9eaff221db0fb7abb5dd6c2e9a9c3c11958
qkrwlgh0369 <37234374+qkrwlgh0369@users.noreply.github.com>
2021-05-12 23:07:15 +0900

    update select level

--- Assets/Scripts/LevelManager.cs
+++ Assets/Scripts/LevelManager.cs
@@ -4,10 +4,11 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.UI;
+using TMPro;
 
 public class LevelManager : MonoBehaviour
 {
-    public int[] LevelScore;
+    public int[] LevelScore = new int[10];
     public int level;
 
     // Start is called before the first frame update
@@ -16,26 +17,23 @@ public class LevelManager : MonoBehaviour
         // 1,2,3 => 스코어, 0 => 현재 스테이지, -1 => 잠김
 
         // 초기 설정
-        if (!PlayerPrefs.HasKey("stage1"))
-            LevelScore = new int[] { 0, -1, -1, -1, -1 };
-        
-        // stage1 완료 기록 보유할 때
-        else
+        LevelScore = new int[] { 0, -1, -1, -1, -1, -1, -1, -1, -1};
+
+        // 스테이지 스코어 불러옴
+        for (int i = 0; i < LevelScore.Length; i++)
         {
-            // 스테이지 스코어 불러옴
-            for (int i = 0; i < 5; i++)
+            int stage = i + 1;
+            if (PlayerPrefs.HasKey("stage" + stage))
             {
-                int stage = i + 1;
-                if (PlayerPrefs.HasKey("stage" + stage))
-                {
-                    LevelScore[i] = PlayerPrefs.GetInt("stage" + stage);
-                }
-                else
-                {
-                    LevelScore[i] = -1;
-                }
+                LevelScore[i] = PlayerPrefs.GetInt("stage" + stage);
+            }
+            else
+            {
+                LevelScore[i] = -1;
             }
         }
+        if (LevelScore[0] == -1) LevelScore[0] = 0;
+        
 
 
         for (int i=0; i < LevelScore.Length; i++)
@@ -45,15 +43,29 @@ public class LevelManager : MonoBehaviour
 
             if(LevelScore[i] > 0)
             {
-                Image scoreImage = levelObject.transform.GetChild(1).GetComponent<Image>();
+                Image scoreImage = levelObject.transform.GetChild(1).GetChild(0).GetComponent<Image>();
                 scoreImage.sprite = Resources.Load<Sprite>("star"+LevelScore[i]);
-                scoreImage.color = new Color(1, 1, 1, 1);
+                TextMeshProUGUI levelText = levelObject.transform.GetChild(1).GetChild(1).GetComponent<TextMeshProUGUI>();
+                levelText.text = "<#636363>" + levelObject.transform.name + "</color>";
+                levelText.raycastTarget = false;
+            }
+
+            if (LevelScore[i] == 0)
+            {
+                Image scoreImage = levelObject.transform.GetChild(1).GetChild(0).GetComponent<Image>();
+                scoreImage.sprite = Resources.Load<Sprite>("star0");
+                TextMeshProUGUI levelText = levelObject.transform.GetChild(1).GetChild(1).GetComponent<TextMeshProUGUI>();
+                levelText.text = levelObject.transform.name;
+                levelText.raycastTarget = false;
+                Image buttonImage = levelObject.transform.GetChild(0).GetComponent<Image>();
+                buttonImage.sprite = Resources.Load<Sprite>("frame_stage_current");
             }
 
             if (LevelScore[i] < 0)
             {
                 Button levelBtn = levelObject.transform.GetChild(0).GetComponent<Button>();
                 levelBtn.interactable = false;
+                levelObject.transform.GetChild(1).gameObject.SetActive(false);
             }
         }
     }

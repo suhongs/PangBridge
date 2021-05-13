9c469ce84461da7ecde45305196cf6bbecfbb884
Booyoung Lee <nutbooyoung@naver.com>
2021-05-13 18:31:40 +0900

    포탈/쿠션블럭 파티클 시스템 적용

--- Assets/Scripts/Object_Cushion.cs
+++ Assets/Scripts/Object_Cushion.cs
@@ -4,10 +4,11 @@ using UnityEngine;
 
 public class Object_Cushion : MonoBehaviour
 {
+    private GameObject nodeParticle;
     // Start is called before the first frame update
     void Start()
     {
-        
+        nodeParticle = Resources.Load("Prefab/Particles/Cube_Cushion") as GameObject;
     }
 
     // Update is called once per frame
@@ -20,6 +21,9 @@ public class Object_Cushion : MonoBehaviour
     {
         if (other.gameObject.tag == "Player")
         {
+            Vector3 colpoint = other.contacts[0].point;
+            Instantiate(nodeParticle, new Vector3(colpoint.x, colpoint.y, -1f), transform.rotation);
+
             Rigidbody rigid = other.gameObject.GetComponent<Rigidbody>();
 
             rigid.velocity = new Vector3(0, 0, 0);
